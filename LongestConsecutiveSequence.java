76d23e23efaa2c27024209dcea513cf2d1332360
Lucas Aguiar <vintesetelucas@gmail.com>
2025-11-03 10:46:19 -0300

    implement longest consecutive sequence algorithm

---
+++ LongestConsecutiveSequence.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+
+public class LongestConsecutiveSequence {
+
+    public static int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) set.add(num);
+        int longestStreak = 0;
+        for (int num : set) {
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+                while (set.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+        return longestStreak;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {11, 666, 33, 7, 10, 48, 24, 12, 72, 22};
+        System.out.println(longestConsecutive(nums));
+    }
+
+}
