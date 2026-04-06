698ee4b1c247b186b861037d9e3a57d9d22e5af0
Lucas Aguiar <vintesetelucas@gmail.com>
2026-04-06 11:33:23 -0300

    refactor: simplify TopKFrequentElements solution (#347)

--- 347/TopKFrequentElements.java
+++ 347/TopKFrequentElements.java
@@ -1,38 +1,36 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class TopKFrequentElements {
 
     public static int[] topKFrequentElements(int[] nums, int k) {
-
-        Map<Integer, Integer> freqMap = new HashMap<>();
-        for (int num : nums) freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
-
-        List<Integer>[] buckets = new ArrayList[nums.length + 1];
-        for (int i = 0; i < buckets.length; i++) buckets[i] = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) buckets[entry.getValue()].add(entry.getKey());
-
-        List<Integer> resultList = new ArrayList<>();
-        for (int i = buckets.length - 1; i >= 0 && resultList.size() < k; i--) if (!buckets[i].isEmpty()) resultList.addAll(buckets[i]);
-
+        Map<Integer, Integer> map = new HashMap<>();
+        ArrayList<ArrayList<Integer>> buckets = new ArrayList<>();
+        for (int num : nums) map.merge(num, 1, Integer::sum);
+        for (int i = 0; i <= nums.length; i++) buckets.add(new ArrayList<>());
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int key = entry.getKey();
+            int value = entry.getValue();
+            buckets.get(value).add(key);
+        }
         int[] result = new int[k];
-        for (int i = 0; i < k; i++) result[i] = resultList.get(i);
+        int idx = 0;
+        for (int i = buckets.size() - 1; i >= 0 && idx < k; i--) {
+            for (int num : buckets.get(i)) {
+                result[idx++] = num;
+                if (idx == k) break;
+            }
+        }
         return result;
-
     }
 
     public static void main(String[] args) {
         int[] nums = {1, 1, 1, 2, 2, 3, 4, 4, 4, 4};
         int k = 2;
-        int[] result = topKFrequentElements(nums, k);
-        System.out.print("[");
-        for (int i = 0; i < result.length; i++) {
-            System.out.print(result[i]);
-            if (i < result.length - 1) System.out.print(", ");
-        }
-        System.out.print("]");
+        System.out.printf("Elements: %s, Frequence: %s%n", Arrays.toString(nums), k);
+        System.out.printf("Top Frequence: %s", Arrays.toString(topKFrequentElements(nums, k)));
     }
 
 }
